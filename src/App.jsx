ba483886120d9d26d331e4a46e7e6cc5d2c84763
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-26 14:22:45 +0300

    fetch jobs from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import {useEffect, useState, useeffect} from 'react'
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([])
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+  
+  
+  if(isLoading){
+    return(
+      <section className='jobs-center'>
+        <div className="loading"></div>
+      </section>
+    )
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
